bfad7b72f1188825e2a37224bb22555a901cfdb4
yw377 <yw377@duke.edu>
2020-09-11 11:48:47 -0400

    .

--- 036_tests_subseq/test-subseq.c
+++ 036_tests_subseq/test-subseq.c
@@ -5,7 +5,7 @@ size_t maxSeq(int * array, size_t n);
 
 int test(int * array, size_t n, size_t ans) {
   if (maxSeq(array, n) != ans) {
-    printf("fail to pass the test");
+    printf("fail to pass the test: got %zu instead of %zu\n", maxSeq(array, n), ans);
     return 0;
   }
   return 1;
@@ -14,13 +14,13 @@ int test(int * array, size_t n, size_t ans) {
 int main() {
   int myArray1[] = {1, 3, 2, 4};
   int myArray2[] = {1, 1, 2, 3};
-  int myArray3[] = {-1,2,6};
-  int myArray4[] = {-3,-2,-1};
-  int myArray5[] = {1,1};
+  int myArray3[] = {-1, 2, 6};
+  int myArray4[] = {-3, -2, -1};
+  int myArray5[] = {1, 1};
   int myArray7[] = {};
 
-  int ind = test(myArray1,4,2) * test(myArray3,3,3) * test(myArray2, 4, 3) * test(myArray4, 3, 3) \
-  * test(myArray5, 2, 1) *test(myArray7, 0, 0);
+  int ind = test(myArray1, 4, 2) * test(myArray3, 3, 3) * test(myArray2, 4, 3) *
+            test(myArray4, 3, 3) * test(myArray5, 2, 1) * test(myArray7, 0, 0);
   if (ind == 0) {
     return EXIT_FAILURE;
   }
