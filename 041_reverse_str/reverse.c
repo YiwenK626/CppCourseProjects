2fd88c1c5bae6c34c1930f1c39efa1d6d59ba9a3
yw377 <yw377@duke.edu>
2020-09-15 18:34:21 -0400

    note the index

--- 041_reverse_str/reverse.c
+++ 041_reverse_str/reverse.c
@@ -3,7 +3,16 @@
 #include <string.h>
 
 void reverse(char * str) {
-  //WRITE ME!
+  // store str at another place str_temp
+  // read the i char of str_temp and assign it to n-i char of str
+  int n = strlen(str);
+  char dest[n];
+  const char * src = str;
+  // printf("%s\n", str);
+  char * str_temp = strncpy(dest, src, n);
+  for (int i = 0; i < n; i++) {
+    str[i] = str_temp[n - 1 - i];
+  }
 }
 
 int main(void) {
@@ -13,7 +22,8 @@ int main(void) {
   char str3[] = "Captain's log, Stardate 42523.7";
   char str4[] = "Hello, my name is Inigo Montoya.";
   char str5[] = "You can be my wingman anyday!";
-  char str6[] = "Executor Selendis! Unleash the full power of your forces! There may be no tomorrow!";
+  char str6[] = "Executor Selendis! Unleash the full power of your forces! There may be "
+                "no tomorrow!";
   char * array[] = {str0, str1, str2, str3, str4, str5, str6};
   for (int i = 0; i < 7; i++) {
     reverse(array[i]);
