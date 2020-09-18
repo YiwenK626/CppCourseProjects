fd48be2f6a429a82e9768fc8cb99c05b668fefbb
yw377 <yw377@duke.edu>
2020-09-18 17:43:43 -0400

    .

--- 047_break_encr/breaker.c
+++ 047_break_encr/breaker.c
@@ -31,7 +31,7 @@ int freq(FILE * f) {
     key = 0;
   }
   else {
-    if (maxind(array, 26) < 'e'- 'a') {
+    if (maxind(array, 26) < 'e' - 'a') {
       key = 26 + maxind(array, 26) + 'a' - 'e';
     }
     else {
@@ -41,36 +41,36 @@ int freq(FILE * f) {
   return key;
 }
 
-  void unencrypt(FILE * f, int key) {
-    int c;
-    while ((c = fgetc(f)) != EOF) {
-      if (isalpha(c)) {
-        printf("used to be %c\n", c);
-        c -= key;
-      }
-      printf("should be %c\n", c);
+void unencrypt(FILE * f, int key) {
+  int c;
+  while ((c = fgetc(f)) != EOF) {
+    if (isalpha(c)) {
+      printf("used to be %c\n", c);
+      c -= key;
     }
+    printf("should be %c\n", c);
+  }
+}
+
+int main(int argc, char ** argv) {
+  if (argc != 2) {
+    fprintf(stderr, "Usage: input file name\n");
+    return EXIT_FAILURE;
   }
 
-  int main(int argc, char ** argv) {
-    if (argc != 2) {
-      fprintf(stderr, "Usage: input file name\n");
-      return EXIT_FAILURE;
-    }
-
-    FILE * f = fopen(argv[1], "r");
-    if (f == NULL) {
-      perror("Could not open the file");
-      return EXIT_FAILURE;
-    }
-    int key = freq(f);
-    //unencrypted with key
-    //unencrypt(f, key);
-
-    if (fclose(f) != 0) {
-      perror("Failed to close the input file");
-      return EXIT_FAILURE;
-    }
-    printf("%d\n", key);
-    return EXIT_SUCCESS;
+  FILE * f = fopen(argv[1], "r");
+  if (f == NULL) {
+    perror("Could not open the file");
+    return EXIT_FAILURE;
   }
+  int key = freq(f);
+  //unencrypted with key
+  //unencrypt(f, key);
+
+  if (fclose(f) != 0) {
+    perror("Failed to close the input file");
+    return EXIT_FAILURE;
+  }
+  printf("%d\n", key);
+  return EXIT_SUCCESS;
+}
