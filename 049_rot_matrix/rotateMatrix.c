aa5e06b64e2d0431432d1a3da93b47913d1581c5
yw377 <yw377@duke.edu>
2020-09-19 01:35:15 -0400

    leak & error

--- 049_rot_matrix/rotateMatrix.c
+++ 049_rot_matrix/rotateMatrix.c
@@ -64,56 +64,11 @@ int main(int argc, char ** argv) {
   }
 
   FILE * f = fopen(argv[1], "r");
-
-  int c;
-  int i = 0;
-  int j = 9;
-
-  int matrix[11][11] = {0};
-  while ((c = fgetc(f)) != EOF) {
-    if (c > 127) {
-      fprintf(stderr, "unprintable char\n");
-      return EXIT_FAILURE;
-    }
-
-    if (j < 0) {
-      fprintf(stderr, "too much lines\n");
-      return EXIT_FAILURE;
-    }
-
-    // printf("%c", c);
-    matrix[i][j] = c;
-    i++;
-
-    if (i == 11) {
-      j--;
-      i = 0;
-    }
-  }
-  // i j should exit loop as 0 and -1
-
-  if (j >= 0) {
-    fprintf(stderr, "too few lines\n");
+  int exit = tomatrix(f);
+  if (exit !=0) {
+    fprintf(stderr, "EXIT with error\n");
     return EXIT_FAILURE;
-  }
-
-  for (int k = 0; k < 10; k++) {
-    if (matrix[10][k] != '\n') {
-      fprintf(stderr, "too much chars\n");
-      return EXIT_FAILURE;
     }
-  }
-
-  for (int a = 0; a < 10; a++) {
-    matrix[a][10] = '\n';
-    for (int b = 0; b < 11; b++) {
-      if (matrix[a][b] == '\n' && b < 10) {
-        fprintf(stderr, "too few chars\n");
-        return EXIT_FAILURE;
-      }
-      printf("%c", matrix[a][b]);
-    }
-  }
 
   if (f == NULL) {
     perror("could not open file");
