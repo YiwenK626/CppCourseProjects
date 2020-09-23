8d7da83cb25e121f3becfd40016e8806015834fa
yw377 <yw377@duke.edu>
2020-09-23 19:26:50 -0400

    3rd

--- 043_eval1/pandemic.c
+++ 043_eval1/pandemic.c
@@ -41,14 +41,22 @@ country_t parseLine(char * line) {
     printf("Illegal input: population starts with zero or space\n");
     exit(EXIT_FAILURE);
   }
-  char arr[15] = {0};
-  for (int i = 0; i < 15; i++) {
-    //error: invalid number with non-digits
-    if (isdigit(*ptr) == 0) {
-      printf("Illegal input: population contains char\n");
-      exit(EXIT_FAILURE);
-    }
+  char arr[11] = {0};
+  for (int i = 0; i < 11; i++) {
     if (*ptr != '\n') {  //read every digit into arr[]
+
+      //error: invalid number
+      if (i > 9) {
+        printf(
+            "Illegal input: too large country population\n");  //when the number is larger than 1000 billions
+        exit(EXIT_FAILURE);
+      }
+
+      //error: invalid number
+      if (isdigit(*ptr) == 0) {
+        printf("Illegal input: population contains char\n");
+        exit(EXIT_FAILURE);
+      }
       arr[i] = *ptr;
       ptr++;
     }
@@ -64,25 +72,22 @@ country_t parseLine(char * line) {
 }
 
 void calcRunningAvg(unsigned * data, size_t n_days, double * avg) {
-  //unmatched country more or less
-  //null add input
-
-  for (int i = 0; i < n_days - 6; i++) {
+  for (int i = 0; i < n_days - 6;
+       i++) {  //for each 7 days calculate sum and move to the next day as startpoint
     int sum = 0;
     for (int j = i; j < i + 7; j++) {
       sum += data[j];
     }
-    avg[i] = sum / 7.0;
+    avg[i] = sum / 7.0;  //calculate 7-day moving average
   }
 }
 
 void calcCumulative(unsigned * data, size_t n_days, uint64_t pop, double * cum) {
-  //unmatched coutry more or less
-  //null add input
   int sum = 0;
-  for (int i = 0; i < n_days; i++) {
+  for (int i = 0; i < n_days;
+       i++) {  //calculate cumulative cases among all population as days pass
     sum += data[i];
-    cum[i] = sum / (pop / 100000.0);
+    cum[i] = sum / (pop / 100000.0);  //calculate cases per 1000000 people
   }
 }
 
@@ -94,9 +99,10 @@ void printCountryWithMax(country_t * countries,
   int ind_country = 0;
   for (size_t i = 0; i < n_countries; i++) {
     for (size_t j = 0; j < n_days; j++) {
-      if (data[i][j] > number_cases) {
+      if (data[i][j] >
+          number_cases) {  //update number_cases by scanning each iteam in the matrix
         number_cases = data[i][j];
-        ind_country = i;
+        ind_country = i;  //once find a larger one, store its row (i.e. country) index
       }
     }
   }
