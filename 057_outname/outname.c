acd25260bba777f31d1518e4626e8f92179a0266
yw377 <yw377@duke.edu>
2020-10-03 15:46:45 -0400

    .

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -1,8 +1,15 @@
+#include "outname.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  char * suffix = ".counts";
+  size_t n = strlen(inputName) + strlen(suffix) + 1;
+  char * output = malloc(n);
+  strcpy(output, inputName);
+  strcat(output, suffix);
+  return output;
 }
