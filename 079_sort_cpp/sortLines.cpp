1d320b894e7c5d01712cea9ab7ceb1df50806a56
yw377 <yw377@duke.edu>
2020-10-17 19:27:45 -0400

    .

---
+++ 079_sort_cpp/sortLines.cpp
@@ -0,0 +1,55 @@
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+void printVector(std::vector<std::string> v) {
+  std::vector<std::string>::iterator it = v.begin();
+  while (it != v.end()) {
+    std::cout << *it << "\n";
+    ++it;
+  }
+}
+
+int main(int argc, char ** argv) {
+  if (argc == 1) {
+    std::string line;
+    std::vector<std::string> strV;
+    std::getline(std::cin, line);
+
+    while (!std::cin.eof()) {
+      strV.push_back(line);
+      std::getline(std::cin, line);
+    }
+    std::sort(strV.begin(), strV.end());
+    printVector(strV);
+  }
+
+  if (argc > 1) {
+    for (int i = 1; i < argc; i++) {
+      std::ifstream fname;
+      fname.open(argv[i]);
+      if (!fname) {
+        std::cout << "fail to open" << argv[i] << std::endl;
+        exit(EXIT_FAILURE);
+      }
+
+      std::string line;
+      std::vector<std::string> strV;
+      std::getline(fname, line);
+
+      while (!fname.eof()) {
+        strV.push_back(line);
+        std::getline(fname, line);
+      }
+      std::sort(strV.begin(), strV.end());
+      printVector(strV);
+      fname.close();
+    }
+  }
+
+  return EXIT_SUCCESS;
+}
