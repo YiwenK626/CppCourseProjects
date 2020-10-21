ff720c96fc7d1f20e68be7be5efcb5f9e08cb04c
yw377 <yw377@duke.edu>
2020-10-20 23:07:31 -0400

    .

---
+++ 083_expr_eval1/expr.h
@@ -0,0 +1,42 @@
+#define __EXPR_H__
+
+#include <iostream>
+#include <sstream>
+class Expression {
+ public:
+  virtual std::string toString() const = 0;
+  virtual ~Expression(){};
+};
+
+class NumExpression : public Expression {
+  long int Num;
+
+ public:
+  NumExpression(long num) : Num(num) {}
+  virtual std::string toString() const {
+    std::stringstream str;
+    str << Num;
+
+    std::string expr;
+    str >> expr;
+    return expr;
+  }
+  virtual ~NumExpression(){};
+};
+
+class PlusExpression : public Expression {
+  Expression * lhs;
+  Expression * rhs;
+
+ public:
+  PlusExpression(Expression * lhs, Expression * rhs) : lhs(lhs), rhs(rhs) {}
+  virtual std::string toString() const {
+    std::stringstream str;
+    str << "(" << lhs->toString() << " + " << rhs->toString() << ")" << std::endl;
+
+    std::string expr;
+    str >> expr;
+    return expr;
+  }
+  virtual ~PlusExpression(){};
+};
