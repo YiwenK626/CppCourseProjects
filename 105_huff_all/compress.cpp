d0889a1176ba76e31476ecc730a68805e929c55c
yw377 <yw377@duke.edu>
2020-11-15 16:48:20 -0500

    md5 hash incorrect

--- 105_huff_all/compress.cpp
+++ 105_huff_all/compress.cpp
@@ -1,19 +1,22 @@
-#include <iostream>
-#include <stdio.h>
+#include <assert.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <fstream>
+#include <functional>
+#include <iostream>
 #include <map>
 #include <queue>
-#include <assert.h>
-#include <functional>
-#include <stdlib.h>
-#include "readFreq.h"
+#include <sstream>
+
 #include "node.h"
+#include "readFreq.h"
 
-
-
-void writeHeader(BitFileWriter * bfw, const std::map<unsigned,BitString> &theMap) {
-  for (int i =0 ; i < 257; i++) {
-    std::map<unsigned,BitString>::const_iterator it = theMap.find(i);
+using namespace std;
+void writeHeader(BitFileWriter * bfw, const std::map<unsigned, BitString> & theMap) {
+  for (int i = 0; i < 257; i++) {
+    std::map<unsigned, BitString>::const_iterator it = theMap.find(i);
     if (it != theMap.end()) {
       bfw->writeByte(it->second.size());
       bfw->writeBitString(it->second);
@@ -24,34 +27,53 @@ void writeHeader(BitFileWriter * bfw, const std::map<unsigned,BitString> &theMap
   }
 }
 
-void writeCompressedOutput(const char* inFile,
-			   const char *outFile,
-			   const std::map<unsigned,BitString> &theMap ){
+void writeCompressedOutput(const char * inFile,
+                           const char * outFile,
+                           const std::map<unsigned, BitString> & theMap) {
   BitFileWriter bfw(outFile);
-  writeHeader(&bfw,theMap);
+  writeHeader(&bfw, theMap);
 
   //WRITE YOUR CODE HERE!
   //open the input file for reading
-
+  ifstream f(inFile);
   //You need to read the input file, lookup the characters in the map,
   //and write the proper bit string with the BitFileWriter
-
+  char c;
+  while (!f.get(c).eof()) {
+    unsigned char y = c;
+    if (theMap.find(y) == theMap.end()) {
+      printf("%u", y);
+      cerr << "error in the Map!\n";
+      exit(EXIT_FAILURE);
+    }
+    BitString b = (theMap.find(y)->second);
+    bfw.writeBitString(b);
+  }
   //dont forget to lookup 256 for the EOF marker, and write it out.
-
-  //BitFileWriter will close the output file in its destructor
+  assert(theMap.find(256) != theMap.end());
+  bfw.writeBitString((theMap.find(EOF))->second);
+  //bitfilewriter will close the output file in its destructor
   //but you probably need to close your input file.
+  f.close();
 }
 
 int main(int argc, char ** argv) {
   if (argc != 3) {
-    fprintf(stderr,"Usage: compress input output\n");
+    fprintf(stderr, "usage: compress input output\n");
     return EXIT_FAILURE;
   }
-  //WRITE YOUR CODE HERE
-  //Implement main
-  //hint 1: most of the work is already done. 
+  //write your code here
+  //implement main
+  //hint 1: most of the work is already done.
   //hint 2: you can look at the main from the previous tester for 90% of this
-
+  uint64_t * counts = readFrequencies(argv[1]);
+  Node * tree = buildTree(counts);
+  map<unsigned, BitString> theMap;
+  BitString b;
+  tree->buildMap(b, theMap);
+  writeCompressedOutput(argv[1], argv[2], theMap);
+  delete[] counts;
+  delete tree;
 
   return EXIT_SUCCESS;
 }
