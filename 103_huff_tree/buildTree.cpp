ce6f3fffa680d2d365a1184531b35ae8e6225648
yw377 <yw377@duke.edu>
2020-11-15 12:02:24 -0500

    l > r

--- 103_huff_tree/buildTree.cpp
+++ 103_huff_tree/buildTree.cpp
@@ -1,5 +1,25 @@
+#include <iostream>
+
 #include "node.h"
 
-Node *  buildTree(uint64_t * counts) {
+using namespace std;
+Node * buildTree(uint64_t * counts) {
   //WRITE ME!
-}  
+  // create a priority queue
+  priority_queue_t pq;
+  // read input
+  // add new nodes to queue
+  for (size_t i = 0; i < 257; i++) {
+    if (counts[i] != 0) {
+      pq.push(new Node(i, counts[i]));
+    }
+  }
+  while (pq.size() != 1) {
+    Node * l = pq.top();
+    pq.pop();
+    Node * r = pq.top();
+    pq.pop();
+    pq.push(new Node(l, r));
+  }
+  return pq.top();
+}
