f9338cc416737ffc0bfd1ec34a7d4452374cb862
yw377 <yw377@duke.edu>
2020-11-16 13:03:06 -0500

    cleaned and commented

--- 089_eval3/cyoa-step4.cpp
+++ 089_eval3/cyoa-step4.cpp
@@ -8,52 +8,30 @@
 
 using namespace std;
 
-// one argv of dire
+// step 4: get winning route //
+
 int main(int argc, char ** argv) {
   if (argc != 2) {
     cerr << "incorrect input argruments!\n";
     exit(EXIT_FAILURE);
   }
 
+  // get reachable pages from 1-N pages
   vector<Page> pages = getPages(argv[1]);
   vector<Page> rpages = getRpages(pages);
 
   // find a WIN page
   int win = findWIN(rpages);
+  // no WIN page
   if (win == -1) {
-    // no WIN page
     cout << "There is no way to win\n";
+    return EXIT_SUCCESS;
   }
   // has a WIN page
   else {
     pRoute(rpages, win);
     cout << "Page " << win << " WIN\n";
   }
-}
-
-void pRoute(vector<Page> rpages, unsigned int endpoint) {
-  if (endpoint != 1) {
-    vector<unsigned int> ans = findRef(rpages, endpoint);
-    pRoute(rpages, ans[0]);
-
-    cout << "Page " << ans[0] << " Choice " << ans[1] << endl;
-  }
-}
-
-vector<unsigned int> findRef(vector<Page> rpages, unsigned int endpoint) {
-  for (unsigned int i = 0; i < rpages.size(); i++) {
-    vector<unsigned int> choices = rpages[i].getChoices();
-    unsigned int num = findV(choices, endpoint);
-    if (num > 0) {
-      vector<unsigned int> ans(2, 0);
-      ans[0] = i + 1;
-      ans[1] = num;
-      return ans;
-    }
-  }
-
-  cerr << "unreachable becomes reachable\n";
-  exit(EXIT_FAILURE);
 }
 
 int findWIN(vector<Page> rpages) {
@@ -64,4 +42,32 @@ int findWIN(vector<Page> rpages) {
     }
   }
   return -1;
+}
+
+void pRoute(vector<Page> rpages, unsigned int endpoint) {
+  if (endpoint != 1) {
+    vector<unsigned int> ans = findRef(rpages, endpoint);
+    // print route to former page recursively
+    pRoute(rpages, ans[0]);
+    cout << "Page " << ans[0] << " Choice " << ans[1] << endl;
+  }
+}
+
+// get the former page of endpoint AND the serial numer of endpoint
+vector<unsigned int> findRef(vector<Page> rpages, unsigned int endpoint) {
+  for (unsigned int i = 0; i < rpages.size(); i++) {
+    vector<unsigned int> choices = rpages[i].getChoices();
+    // get the position if endpoint appears in a page's choices
+    unsigned int num = findV(choices, endpoint);
+    if (num > 0) {
+      vector<unsigned int> ans(2, 0);
+      ans[0] = i + 1;
+      ans[1] = num;
+      return ans;
+    }
+  }
+
+  // can't find a former page for reachable pages
+  cerr << "unreachable becomes reachable\n";
+  exit(EXIT_FAILURE);
 }
